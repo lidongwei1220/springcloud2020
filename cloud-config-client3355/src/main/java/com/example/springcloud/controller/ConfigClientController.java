ff70fe75ade2b2a025edd6e8cfd38cfaf5e9fde8
lidongwei <1490619147@qq.com>
2020-06-23 15:42:41 +0800

    'dd'

--- cloud-config-client3355/src/main/java/com/example/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/example/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.example.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/6/22 9:49
  */
 @RestController
+@RefreshScope     //发送(curl -X POST"http://localhost:3355/actuator/refresh")刷新，不需要重启
 public class ConfigClientController {
 
-    @Value("${config.info}")
-    private String configInfo;
+//    @Value("${config.info}")
+//    private String configInfo;
 
     @GetMapping("/configInfo")
     public String getConfigInfo() {
-        return configInfo;
-
-
+        return "configInfo";
     }
 }
